a069ee46bf464e22b7d78b2e6733d4208706e50a
shioimm <shioi.mm@gmail.com>
2019-01-21 19:33:52 +0900

    Add image_url column to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,13 @@ class User < ApplicationRecord
   def self.find_or_create_by_oauth(auth)
     uid         = auth[:uid]
     name        = auth[:info][:nickname]
+    image_url   = auth[:info][:image]
     oauth_token = auth[:credentials][:token]
 
     self.find_or_create_by(uid: uid) do |user|
       user.uid         = uid
       user.name        = name
+      user.image_url   = image_url
       user.oauth_token = oauth_token
     end
   end
