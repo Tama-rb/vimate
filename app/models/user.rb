6f25437370da045466aa907e98b292a66b250374
shioimm <shioi.mm@gmail.com>
2019-01-21 19:30:30 +0900

    Add omniauth login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  def self.find_or_create_by_oauth(auth)
+    uid         = auth[:uid]
+    name        = auth[:info][:nickname]
+    oauth_token = auth[:credentials][:token]
+
+    self.find_or_create_by(uid: uid) do |user|
+      user.uid         = uid
+      user.name        = name
+      user.oauth_token = oauth_token
+    end
+  end
 end
